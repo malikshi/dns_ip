4b9e2dab81b84104b5a841be1356117c0b68ebda
Malik Al <kidznightserver@gmail.com>
2024-08-04 12:58:23 +0700

    tests

---
+++ getDomainDOH.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Fetch Lists
+curl -s https://raw.githubusercontent.com/dibdot/DoH-IP-blocklists/master/doh-domains.txt > doh-domains1.txt
+curl -s https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/doh.txt > doh-domains2.txt
+
+# Merge and Deduplicate
+cat doh-domains1.txt doh-domains2.txt | sort -u | sed '/^[[:space:]]*$/d' | sed '/#/d' > domains-doh.txt
+
+# Cleanup
+rm doh-domains1.txt doh-domains2.txt
---
+++ resolveDOH.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+domains = []
+with open('domains-doh.txt', 'r') as f:
+    domains = f.read().splitlines()
+
+with open('ipv4-doh.txt', 'w') as ipv4_file, open('ipv6-doh.txt', 'w') as ipv6_file:
+    for domain in domains:
+        # Request for IPv4 (A) records
+        url_ipv4 = f"https://dns.google/resolve?name={domain}&type=A"
+        response_ipv4 = requests.get(url_ipv4)
+        if response_ipv4.status_code == 200:
+            data_ipv4 = json.loads(response_ipv4.text)
+            if "Answer" in data_ipv4:
+                for answer in data_ipv4["Answer"]:
+                    ipv4_file.write(f"{answer['data']} #{domain}\n")
+
+        # Request for IPv6 (AAAA) records
+        url_ipv6 = f"https://dns.google/resolve?name={domain}&type=AAAA"
+        response_ipv6 = requests.get(url_ipv6)
+        if response_ipv6.status_code == 200:
+            data_ipv6 = json.loads(response_ipv6.text)
+            if "Answer" in data_ipv6:
+                for answer in data_ipv6["Answer"]:
+                    ipv6_file.write(f"{answer['data']} #{domain}\n")
