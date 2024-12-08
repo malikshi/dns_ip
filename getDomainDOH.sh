7fea5e049aca6782b68de0a107b27535eef15bda
Malik Al <kidznightserver@gmail.com>
2024-12-08 22:40:06 +0700

    remove apple and icloud from list

--- getDomainDOH.sh
+++ getDomainDOH.sh
@@ -5,7 +5,7 @@ curl -s https://raw.githubusercontent.com/dibdot/DoH-IP-blocklists/master/doh-do
 curl -s https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/doh.txt > doh-domains2.txt
 
 # Merge and Deduplicate
-cat doh-domains1.txt doh-domains2.txt | sort -u | sed '/^[[:space:]]*$/d' | sed '/#/d' > domains-doh.txt
+cat doh-domains1.txt doh-domains2.txt | sort -u | sed '/^[[:space:]]*$/d' | sed '/#/d' | grep -v -i 'apple' | grep -v -i 'icloud' > domains-doh.txt
 
 # Cleanup
 rm doh-domains1.txt doh-domains2.txt
