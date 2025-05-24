d6222e2043240105fd57897ee261e82404b3040e
Ibnu Malik Al Kusaeri <kidznightserver@gmail.com>
2025-05-24 16:28:39 +0700

    exclude vercel

--- filter.py
+++ filter.py
@@ -4,9 +4,11 @@ import requests
 # Step 1: Fetch Cloudflare IPv4 and IPv6 networks
 cloudflare_ipv4_url = "https://www.cloudflare.com/ips-v4"
 cloudflare_ipv6_url = "https://www.cloudflare.com/ips-v6"
+vercel_url = "https://raw.githubusercontent.com/malikshi/dns_ip/main/vercel.txt"
 exclude_ips_url = "https://raw.githubusercontent.com/malikshi/dns_ip/main/exclude.txt"
 cloudflare_ipv4_networks = requests.get(cloudflare_ipv4_url).text.splitlines()
 cloudflare_ipv6_networks = requests.get(cloudflare_ipv6_url).text.splitlines()
+vercel_network = requests.get(vercel_url).text.splitlines()
 exclude_ips_networks = requests.get(exclude_ips_url).text.splitlines()
 
 # Step 2: Read IP addresses from the given URL
@@ -19,7 +21,7 @@ with open(ip_dns_file, "r") as file:
 filtered_ips = []
 for ip in ip_dns_list:
     is_cloudflare_ip = False
-    for network in cloudflare_ipv4_networks + cloudflare_ipv6_networks + exclude_ips_networks:
+    for network in cloudflare_ipv4_networks + cloudflare_ipv6_networks + exclude_ips_networks + vercel_network:
     # for network in exclude_ips_networks:
         if ipaddress.ip_address(ip) in ipaddress.ip_network(network):
             is_cloudflare_ip = True
